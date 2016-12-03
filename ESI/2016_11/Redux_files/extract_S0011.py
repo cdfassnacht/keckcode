39d691bfedc3abf1a11904657d264c2209bf7ed2
Chris Fassnacht <cdfassnacht@ucdavis.edu>
2016-12-03 13:39:43 -0800

    Updates of 2016_11 ESI extraction scripts

--- ESI/2016_11/Redux_files/extract_S0011.py
+++ ESI/2016_11/Redux_files/extract_S0011.py
@@ -7,7 +7,7 @@ frames = [35,36]
 apcent = [0.,]
 aplab = ['all']
 #aplab = ['A','B']
-nsig = 1.5 # Width of aperture in terms of sigma.  Normal value is 1.0
+nsig = 2.5 # Width of aperture in terms of sigma.  Normal value is 1.0
 
 for jj in range(len(aplab)):
-    ow,s,v = extract(fullname,name,frames,jj,apcent,aplab,stdOrderCorr,wid=nsig)
+    extract(fullname,name,frames,jj,apcent,aplab,stdOrderCorr,wid=nsig)
--- ESI/2016_11/Redux_files/extract_S0739.py
+++ ESI/2016_11/Redux_files/extract_S0739.py
@@ -4,10 +4,11 @@ stdOrderCorr = 'orderCorr_Feige110.dat'
 fullname = 'S0739+1350'
 name='S0739'
 frames = [55,56]
-apcent = [0.,]
-aplab = ['all']
-#aplab = ['A','B']
+apcent = [0.,-1.9]
+#aplab = ['all']
+aplab = ['A','B']
 nsig = 1.1 # Width of aperture in terms of sigma.  Normal value is 1.0
 
 for jj in range(len(aplab)):
-    ow,s,v = extract(fullname,name,frames,jj,apcent,aplab,stdOrderCorr,wid=nsig)
+    extract(fullname,name,frames,jj,apcent,aplab,stdOrderCorr,wid=nsig)
+    #ow,s,v = extract(fullname,name,frames,jj,apcent,aplab,stdOrderCorr,wid=nsig)
