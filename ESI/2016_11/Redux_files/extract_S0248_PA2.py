e0ffb41d4e3b57f53e18587687936b685eb36ded
Chris Fassnacht <cdfassnacht@ucdavis.edu>
2016-12-06 20:14:14 -0800

    More spectra from 2016_11 ESI run

--- ESI/2016_11/Redux_files/extract_S0248_PA2.py
+++ ESI/2016_11/Redux_files/extract_S0248_PA2.py
@@ -4,10 +4,10 @@ stdOrderCorr = 'orderCorr_Feige110.dat'
 fullname = 'S0248_PA2'
 name='S0248'
 frames = [41,42]
-apcent = [0.,]
-aplab = ['all']
-#aplab = ['A','B']
-nsig = 1.5 # Width of aperture in terms of sigma.  Normal value is 1.0
+apcent = [0.,-1.18,3.9]
+#aplab = ['all']
+aplab = ['C','D','G2']
+nsig = 0.8 # Width of aperture in terms of sigma.  Normal value is 1.0
 
 for jj in range(len(aplab)):
-    ow,s,v = extract(fullname,name,frames,jj,apcent,aplab,stdOrderCorr,wid=nsig)
+    extract(fullname,name,frames,jj,apcent,aplab,stdOrderCorr,wid=nsig)
