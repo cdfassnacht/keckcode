bf6879a79827fd96d9296c7de251833da6685b68
cdfassnacht <cdfassnacht@ucdavis.edu>
2017-02-27 17:42:48 -0800

    Added new write_gal method to dsimCat class

--- python/dsim_funcs.py
+++ python/dsim_funcs.py
@@ -39,6 +39,7 @@ class dsimCat(cf.Secat):
       self.bandname  = None
       self.starband  = None
       self.selmask   = None
+      self.dstab     = None
 
       """ 
       Read in the catalog and call the superclass initialization (to cf.Secat)
@@ -68,6 +69,71 @@ class dsimCat(cf.Secat):
       #theta = np.zeros(self.nrows)
       #self.dstab = Table([name,pri,theta], names=('name','pri','theta'))
 
+   #----------------------------------------------------------------------
+
+   def write_gal(self, outfile, verbose=False):
+      """
+      Writes out the selected galaxies in the format needed for DSIMULATOR
+      input. 
+
+      NOTE: Many of the parameters that used to be passed to the stand-alone
+      function, e.g., the priority vector, are now contained within the
+      dsimCat structure itself and so no longer have to be explicitly
+      passed in the call.
+      """
+
+      """ 
+      Make sure that the priorities, etc. have been put into the dstab
+      array within the dsimCat structure
+      """
+      if self.dstab is None:
+         print ''
+         print 'ERROR: Cannot write out galaxies without dstab (priorities,etc.)'
+         print ' being set'
+         print ''
+         return
+
+      """ 
+      Check to make sure that the selection mask has been made.  If not, make
+      a mask that selects all of the catalog members.
+      """
+      if self.selmask is None:
+         self.selmask = np.ones(self.nrows,dtype=bool)
+
+      """ Use the selection mask to pull out the selected objects """
+      seldata  = self.data[self.selmask]
+      selradec = self.radec[self.selmask]
+      #selinfo  = self.dstab[self.selmask]
+
+      """ Convert the radec info into the appropriate format """
+      tmpra = selradec.ra.to_string(unit=u.hourangle,decimal=False,sep=':',
+                                    precision=3,pad=True)
+      tmpdec = selradec.dec.to_string(decimal=False,sep=':',precision=3,
+                                      alwayssign=True,pad=True)
+
+      """ 
+      Store the output information in a numpy array for improved (hopefully)
+      speed in writing out the data.
+      """
+      nsel = self.selmask.sum()
+      dfmt = ['S16','S12','S13',float,float,'S2',int,int,int,float]
+      dnames = ['name','ra','dec','equinox','mag','band','pri','samp','sel',
+                'pa']
+      outarr = np.zeros(nsel,dtype={'names':dnames,'formats':dfmt})
+      outarr['name'] = self.dstab['name']
+      outarr['ra'] = tmpra
+      outarr['dec'] = tmpdec
+      outarr['equinox'] += 2000.
+      outarr['mag'] = seldata[self.selband]
+      outarr['band'] = self.selband
+      outarr['pri'] = self.dstab['pri']
+      outarr['samp'] += 1
+      outarr['pa'] = self.dstab['pa']
+      #print outarr
+
+      """ Write to the output file """
+      outfmt = '%-16s %s %s %.1f %5.2f %s %d %d %d %.1f'
+      np.savetxt(outfile,outarr,fmt=outfmt)
 
 #---------------------------------------------------------------------------
 
