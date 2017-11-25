83368fc52b67bda83e352b75295e5bb49969e9c7
Chris Fassnacht <cdfassnacht@ucdavis.edu>
2017-11-24 22:44:37 -0800

    Added functionality to osiris_funcs.py

--- python/osiris_funcs.py
+++ python/osiris_funcs.py
@@ -36,7 +36,8 @@ class osCube(imf.Image):
 
     # -----------------------------------------------------------------------
 
-    def set_imslice(self, imslice=0, hext=0, display=True, mode='xy'):
+    def set_imslice(self, imslice=0, hext=0, display=True, mode='xy',
+                    **kwargs):
         """
         Sets the 2-dimension slice to use for the display functions.
 
@@ -78,27 +79,75 @@ class osCube(imf.Image):
             print('')
             raise IndexError
 
-        self.data = self.hdu[hext].data[:, :, imslice].copy()
+        self.data = np.transpose(self.hdu[hext].data[:, :, imslice].copy())
 
         """ Display the image slice if requested """
         if display:
             self.display(title='Image Plane %d (zero-indexed)' % imslice,
-                         mode=mode)
+                         mode=mode, **kwargs)
 
     # -----------------------------------------------------------------------
 
-    def set_1dspec(self, x, y, hext=0, display=True):
+    def compress_spec(self, wmin, wmax, wmode='slice', combmode='sum',
+                      hext=0, display=True, **kwargs):
+        """
+        Compresses the data cube along the spectral dimension, but only
+        for image slices between some minimum and maximum wavelengths.
+        These wavelength limits (wmin, wmax) can be set either by the slice
+        number or the actual wavelength in Angstrom [wavelength mode is NOT
+        yet implemented].
+
+        The compression can be done either as a sum or as a median.
+
+        The result is a 2-dimensional spatial image, which is stored in the
+        data container and, thus, can be easily displayed.
+        """
+
+        """ First select the image slices to use """
+        if wmode == 'wavelength':
+            print('NOTE: wavelength mode has not yet been implemented')
+            return
+        else:
+            minslice = wmin
+            maxslice = wmax
+            wavmin = self.wav[minslice]
+            wavmax = self.wav[maxslice]
+        print('')
+        print('Data cube will be compressed along the spectral direction')
+        print('  Image slice range: %d - %d' % (minslice, maxslice))
+        print('  Corresponding wavelength range: %8.2f - %8.2f'
+              % (wavmin, wavmax))
+
+        """ Create a temporary cube container """
+        cube = self.hdu[hext].data[:, :, minslice:(maxslice+1)].copy()
+
+        """ Compress the temporary cube along the spectral axis """
+        if combmode == 'median':
+            self.data = np.median(cube, axis=2)
+        else:
+            self.data = cube.sum(axis=2)
+
+        """ Display the result if requested """
+        if display:
+            self.display(**kwargs)
+
+        """ Clean up """
+        del(cube)
+
+    # -----------------------------------------------------------------------
+
+    def set_1dspec(self, x, y, hext=0, display=True, **kwargs):
         """
         Takes a spaxel designated by its (x, y) coordinates and extracts the
         spectral information into a Spec1d container.
         Also plot the 1d spectrum if requested.
         """
 
-        flux = self.hdu[hext].data[y, x, :]
+        flux = self.hdu[hext].data[x, y, :]
         print(self.wav.size, flux.size)
         spec = ss.Spec1d(wav=self.wav, flux=flux)
 
         if display:
-            spec.plot()
+            spec.plot(**kwargs)
 
         return spec
