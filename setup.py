d3c2898f16d3a7a6ca98a204ee88cfaa902abe49
Chris Fassnacht <cdfassnacht@ucdavis.edu>
2018-11-30 21:04:11 -0800

    Another rename of the package -- to keckcode

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     description = 'Code for analyzing data from various Keck instruments',
     long_description = open('README.txt').read(),
     requires = ['numpy', 'scipy', 'astropy', 'matplotlib'],
-    packages = ['keck_code', 'keck_code.osiris'],
+    packages = ['keckcode', 'keckcode.osiris', 'keckcode.esiredux'],
 #    package_dir = {'':'src'},
 #    package_data = {'esi_redux' : ['data/*']}
 )
