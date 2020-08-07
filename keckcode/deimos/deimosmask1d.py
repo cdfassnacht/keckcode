3c169a00b6b6c895b9289efc729018b128b123cc
Patrick Wells <pwells@ucdavis.edu>
2020-08-07 14:41:22 -0700

    intermediate commit

--- keckcode/deimos/deimosmask1d.py
+++ keckcode/deimos/deimosmask1d.py
@@ -225,7 +225,7 @@ class DeimosMask1d(OrderedDict):
 
         for k,v in lines.items():
             if v:
-                self[0].mark_lines(k, z, **kwargs)
+                self[slitid].mark_lines(k, z, **kwargs)
 
     # -----------------------------------------------------------------------
 
